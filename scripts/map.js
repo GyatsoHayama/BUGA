4bad482a88b1def8a66c5df1064676156b5fd08f
GyatsoHayama <1998maximus@gmail.com>
2026-09-23 19:23:46 +0200

    v.04.1

--- scripts/map.js
+++ scripts/map.js
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
         zoomControl: true
     });
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-        attribution: '&copy; OpenStreetMap contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 19
     }).addTo(map);
 
